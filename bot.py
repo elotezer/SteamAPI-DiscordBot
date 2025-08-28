4cc8ebe58e26f9c61838502ef828021fa19bd5e4
Konyári László <konyarilaszlo25@gmail.com>
2025-08-28 14:47:37 +0200

    loaded api keys from .env

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
-from dotenv import load_dotenv
 import discord
+from dotenv import load_dotenv
 import os
+import asyncio
+from discord.ext import commands
+
 load_dotenv()
 
-API_KEY = os.getenv("STEAM_API_KEY")
-
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+STEAM_API_KEY = os.getenv("STEAM_API_KEY")
