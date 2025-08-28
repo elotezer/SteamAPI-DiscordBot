23737db50dce0c5d2640f72d5db65286d6f1ff0b
Konyári László <konyarilaszlo25@gmail.com>
2025-08-28 14:49:33 +0200

    added main entrypoint and cog loader

--- bot.py
+++ bot.py
@@ -13,3 +13,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
+
+async def main():
+    async with bot:
+        await bot.load_extension("steam_cog")
+        await bot.start(DISCORD_TOKEN)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    
